4bdc22a679533c83026919d86a0d05b931ea5999
pedroelir <pedro_r_b777@hotmail.com>
2024-08-04 11:48:30 +0200

    refactor and format

--- timing_issue.py
+++ timing_issue.py
@@ -8,8 +8,22 @@ def calculate_dot_product(size) -> int:
     vector2: list[int] = [random.randint(0, 100) for _ in range(size)]
     return sum([i * j for (i, j) in zip(vector1, vector2)])
 
+
 def product_times_10(num) -> int:
-    return 10*num
+    return 10 * num
+
+
+def custom_sleep(sleep_time: float) -> None:
+    t0: int = time.perf_counter_ns()
+    timeout: float = sleep_time * 1000_000_000
+    while time.perf_counter_ns() - t0 < timeout:
+        pass
+
+
+GET_TIME_CLOCK = "perf_counter"
+get_time_ns = time.perf_counter_ns
+sleep = custom_sleep
+# sleep = time.sleep
 
 
 def measure_time(func, args) -> int:
@@ -17,46 +31,31 @@ def measure_time(func, args) -> int:
     func(*args)
     return get_time_ns() - t0
 
-def custom_sleep(sleep_time:float) -> None:
-    t0: int = time.perf_counter_ns()
-    timeout: float = sleep_time * 1000_000_000
-    while (time.perf_counter_ns() - t0 < timeout):
-        pass
 
+def measure_time_with_brake(func, args):
+    sleep(0.01)
+    return measure_time(func=func, args=args)
 
-
-get_time_clock = "perf_counter"
-get_time_ns = time.perf_counter_ns
-sleep = custom_sleep
-# sleep = time.sleep
 
 def loop_func(func, args, loops):
-    # measurements =[]
-    # for _ in range(loops):
-    #     measurement = measure_time(func=func, args=args)
-    #     measurements.append(measurement)
-    # return measurements
+    return [measure_time(func=func, args=args) for _ in range(loops)]
 
-    return[measure_time(func=func,args=args) for _ in range(loops)]
 
 def loop_func_with_break(func, args, loops):
-    # measurements =[]
-    # for _ in range(loops):
-    #     sleep(1)
-    #     measurement = measure_time(func=func, args=args)
-    #     measurements.append(measurement)
-    # return measurements
-    def measure_time_with_brake(func, args):
-        sleep(1)
-        return measure_time(func=func,args=args)
-    return[measure_time_with_brake(func=func,args=args) for _ in range(loops)]
+
+    return [measure_time_with_brake(func=func, args=args) for _ in range(loops)]
+
+
+def print_py_info():
+    print(f"clock info for {GET_TIME_CLOCK}")
+    print(time.get_clock_info(GET_TIME_CLOCK))
+    print(sys.version_info)
+
 
 def main_print():
     loops = 10
     product_dot_args = (1,)
-    print(f"clock info for {get_time_clock}")
-    print(time.get_clock_info(get_time_clock))
-    print(sys.version_info)
+    print_py_info()
 
     # print("First call calculate_dot_product")
     # print(measure_time(calculate_dot_product, product_dot_args))
@@ -80,19 +79,22 @@ def main_print():
 
 
 def main_time():
-    product_args = (10,)
     global sleep
+    test_func = product_times_10
+    test_args = (10,)
+    loops = 25
     # print("First call product_times_10")
-    initial_call = measure_time(product_times_10,product_args)
-    call_without_break = loop_func(product_times_10, product_args,loops=10)
-    sleep = lambda x:x**x
-    call_with_break_lambda = loop_func_with_break(product_times_10, product_args,loops=10)
+    initial_call = measure_time(test_func, test_args)
+    call_without_break = loop_func(test_func, test_args, loops=loops)
+    sleep = lambda x: x**x
+    call_with_break_lambda = loop_func_with_break(test_func, test_args, loops=loops)
     sleep = custom_sleep
-    call_with_break_custom_sleep = loop_func_with_break(product_times_10, product_args,loops=10)
+    call_with_break_custom_sleep = loop_func_with_break(test_func, test_args, loops=loops)
     sleep = print
-    call_with_break_print = loop_func_with_break(product_times_10, product_args,loops=10)
+    call_with_break_print = loop_func_with_break(test_func, test_args, loops=loops)
     sleep = time.sleep
-    call_with_break_time_sleep = loop_func_with_break(product_times_10, product_args,loops=10)
+    call_with_break_time_sleep = loop_func_with_break(test_func, test_args, loops=loops)
+    print_py_info()
     print(f"{initial_call=},")
     print(f"{call_without_break=},")
     print(f"{call_with_break_lambda=}.")
@@ -102,5 +104,4 @@ def main_time():
 
 
 if __name__ == "__main__":
-    # main_print()
     main_time()
